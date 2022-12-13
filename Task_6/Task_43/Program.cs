151e335756086f8883d6c31eeb8dcbf4b50452e5
Alena <kosoohina@yandex.ru>
2022-12-13 16:31:03 +0300

    Seminar_6

---
+++ Task_6/Task_43/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("k1: ");
+Double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1: ");
+Double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2: ");
+Double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2: ");
+Double b2 = Convert.ToDouble(Console.ReadLine());
+Double x = Math.Round((-(b1 - b2) / (k1 - k2)),3);
+Double y = Math.Round((k1 * x + b1),3);
+Console.WriteLine($"Пересечеybt: ({x};{y})");
---
+++ Task_6/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿int m = int.Parse(Console.ReadLine());
+int[] array = new int[m];
+int k = 0;
+for (int i = 0; i < m; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        k++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(k);
